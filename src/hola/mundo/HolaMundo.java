d95aa48163b971ed468f8534a542091dda903cff
amontufar1 <amontufara1@miumg.edu.gt>
2022-07-22 17:48:47 -0600

    Add examples

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package hola.mundo;
+
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("bienvenido progra 2 2022");
+    
+    }
+    
+    
+}
